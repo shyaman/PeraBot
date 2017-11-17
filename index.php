d4d6479cff4fcfb402bb85676dfa336d0835b2a8
shyaman_madhawa <shayaman321@gmail.com>
2017-11-17 19:35:51 +0530

    db test

--- index.php
+++ index.php
@@ -8,6 +8,13 @@ echo "hello world heee";
   FROM [dbo].[contacts]";
     $qDep = mysqli_query($conn,$qD);
     $DNo = mysqli_fetch_assoc($qDep);
-    echo $DNo['mailAddress'];
+    
+    echo "<table>"; 
+        echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
+        while($row = mysqli_fetch_assoc($result)){  
+            echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['email'] . "</td></tr>";  
+            }
+
+        echo "</table>";
 
 ?>
