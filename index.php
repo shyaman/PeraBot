ebce70c61eb31c70c415bcf0651b16430e2978ac
shyaman_madhawa <shayaman321@gmail.com>
2017-11-17 23:32:56 +0530

    db test5

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
 require_once('inc/connect.php'); 
 echo "hello world heee";
 
-	$qD = "SELECT * FROM contacts";
-    $qDep = $conn->query("qD");
-    
-    echo "<table>"; 
-        echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
-        while($row = $qDep->fetch();){  
-            echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['email'] . "</td></tr>";  
-            }
-
-        echo "</table>";
+	
 
 ?>
