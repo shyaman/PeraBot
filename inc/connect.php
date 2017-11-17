fb9c3873b5f8e8157929d182c7c0aa364b1d5d18
shyaman_madhawa <shayaman321@gmail.com>
2017-11-18 00:13:56 +0530

    db test

--- inc/connect.php
+++ inc/connect.php
@@ -1,19 +1,13 @@
 <?php 
+$dsn = 'mysql:dbname=localdb;host=127.0.0.1:56116;charset=utf8';
+$user = 'azure';
+$password = '6#vWHD_$';
+
 try {
-    $conn = new PDO("sqlsrv:server = tcp:perabot.database.windows.net,1433; Database = perabot", "perabotDev", "per@bot1");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = ("SELECT * FROM contacts");
-	$stat = $conn->query("$sql");
-	$row = $qDep->fetch();
-	echo "$row[0] $row[1] $row[2]";
-
-	
-
-	$conn = NULL;
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
 
 ?>
