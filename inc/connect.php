ff8c0a09f6e9884b62cd832f184ac7d3285a0e0f
shyaman_madhawa <shayaman321@gmail.com>
2017-11-18 00:21:55 +0530

    db test

--- inc/connect.php
+++ inc/connect.php
@@ -1,4 +1,5 @@
 <?php 
+
 $dsn = 'mysql:dbname=localdb;host=127.0.0.1:56116;charset=utf8';
 $user = 'azure';
 $password = '6#vWHD_$';
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
 	require_once('inc/connect.php'); 
 	echo "hello world";
+
+	$sql = ("SELECT * FROM contacts");
+ 	$stat = $conn->query("$sql");
+ 	$row = $qDep->fetch();
+ 	echo "$row[0] $row[1] $row[2]";
+
+ 	$conn = NULL;
 ?>
