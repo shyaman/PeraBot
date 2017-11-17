eeb34af1297128ab7e9c72f4cc2f052b50cddff6
shyaman_madhawa <shayaman321@gmail.com>
2017-11-17 23:42:09 +0530

    db test8

--- inc/connect.php
+++ inc/connect.php
@@ -2,6 +2,21 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:perabot.database.windows.net,1433; Database = perabot", "perabotDev", "per@bot1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = ("SELECT * FROM contacts");
+	$stat = $conn->query("sql");
+	$row = $qDep->fetch();
+	echo "$row[0] $row[1] $row[2]";
+
+	// echo "<table>"; 
+	// echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
+	// while($row = $qDep->fetch()){  
+	// 	echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['email'] . "</td></tr>";  
+	// }
+
+	// echo "</table>";
+
+	$conn = NULL;
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php 
 	require_once('inc/connect.php'); 
-	echo "hello world heee";
-
-	$sql = ("SELECT * FROM contacts");
-	$stat = $conn->query("sql");
-	$row = $qDep->fetch();
-	echo "$row[0] $row[1] $row[2]";
-
-	// echo "<table>"; 
-	// echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
-	// while($row = $qDep->fetch()){  
-	// 	echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['email'] . "</td></tr>";  
-	// }
-
-	// echo "</table>";
-
-	$conn = NULL;
+	echo "hello world";
 ?>
