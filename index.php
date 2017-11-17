906d69d58f9650f48faf9b4af32676fe64b85461
shyaman_madhawa <shayaman321@gmail.com>
2017-11-17 19:29:29 +0530

    db test

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 require_once('inc/connect.php'); 
 echo "hello world heee";
 
+	$qD = "SELECT TOP (1000) [fName]
+      ,[lName]
+      ,[mailAddress]
+  FROM [dbo].[contacts]";
+    $qDep = mysqli_query($conn,$qD);
+    $DNo = mysqli_fetch_assoc($qDep);
+    echo $DNo['mailAddress'];
+
 ?>
