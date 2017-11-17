fdbb6a6c04e80d32cc045d1f27974feca5ef3dc5
shyaman_madhawa <shayaman321@gmail.com>
2017-11-18 00:39:38 +0530

    db test

--- inc/connect.php
+++ inc/connect.php
@@ -1,21 +1,9 @@
 <?php 
+	$connection = mysqli_connect('127.0.0.1:56116','azure','6#vWHD_$','localdb');
 
-$dsn = 'mysql:dbname=localdb;host=127.0.0.1:56116;charset=utf8';
-$user = 'azure';
-$password = '6#vWHD_$';
-
-try {
-    $conn = new PDO($dsn, $user, $password);
-    $sql = ("SELECT * FROM contacts");
- 	$stat = $conn->query("$sql");
- 	$row = $qDep->fetch();
- 	echo "$row[0] $row[1] $row[2]";
-
- 	$conn = NULL;
-
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-    exit;
-}
-
+	if (mysqli_connect_errno()) {
+		die('Database connection failed' . mysqli_connect_errno());
+	}else{
+		echo "connection successful";
+	}
 ?>
