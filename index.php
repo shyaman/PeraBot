03d72af20d0cd789e4d7e5d617eab5731335eed8
shyaman_madhawa <shayaman321@gmail.com>
2017-11-17 23:35:54 +0530

    db test5

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php 
-require_once('inc/connect.php'); 
-echo "hello world heee";
+	require_once('inc/connect.php'); 
+	echo "hello world heee";
 
 	$qD = "SELECT * FROM contacts";
-    $qDep = $conn->query("qD");
-    
-    echo "<table>"; 
-        echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
-        while($row = $qDep->fetch()){  
-            echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['email'] . "</td></tr>";  
-            }
+	$qDep = $conn->query("qD");
 
-        echo "</table>";
+	echo "<table>"; 
+	echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
+	while($row = $qDep->fetch()){  
+		echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['email'] . "</td></tr>";  
+	}
+
+	echo "</table>";
 
 ?>
