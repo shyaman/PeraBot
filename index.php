446f60e163f47f8dfc20e1b7f514ec8f672b7b45
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-19 15:55:52 +0530

    adding information type

--- index.php
+++ index.php
@@ -15,20 +15,16 @@
 				//include ('features/contacInformation');
 
 				$person =  $param->person;	//extract person name
-				$infoType=$param->information-type // extract information type
 				
 				$splitName = explode(' ', $person);
-				
-				$query = "SELECT * FROM contacts WHERE fName = '$splitName[0]' and lName = '$splitName[1]'";
-				$ontactDetail = mysqli_query($connection,$query);
-				
-				//fetch contact details according to information-type
-				if($infoType == 'email address'){
-					$speech = "Email address of $person is {$ontactDetail['mailAddress']}" ;
-				}else if($infoType ==''){
-					$speech = "Email address : {$ontactDetail['mailAddress']}" ;
-				}
-					
+
+				//only search by fName, should be modified
+				$query = "SELECT * FROM contacts WHERE fName = '$splitName[0]'"; 
+			    $result = mysqli_query($connection,$query);
+			    $mail = mysqli_fetch_assoc($result);
+
+				$speech = "Email address of $person is {$mail['mailAddress']}" ;
+
 				//create reponse to the dilogflow and echo it
 				$response = new \stdClass();
 				$response->speech = $speech;
