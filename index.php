439e5e565d11cb3d43d655229776ee66b0d90886
shyaman_madhawa <shayaman321@gmail.com>
2017-11-19 08:40:10 +0530

    webhook test

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php 
-	require_once('inc/connect.php'); 
-	echo "hello world";
+	require_once('inc/connect.php'); //database connect
 
-	$query = "SELECT * FROM contacts"; 
-    $result = mysqli_query($connection,$query);
+	$method  = $_SERVER['REQUEST_METHOD'];
 
-	echo "<table>"; 
-	echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
-	while($row = mysqli_fetch_assoc($result)){  
-		echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['mailAddress'] . "</td></tr>";  
+	if ($method == "POST") {
+		$requestBody = file_get_contents('php://input');
+		$json = json_decode($requestBody);
+
+		$intent = $json->result->metadata->intentName;
+		$param = $json->result->parameters;
+
+		switch ($intent) {
+			case 'get_contact_information':
+				//include ('features/contacInformation');
+
+				$person =  $param->person;
+
+				echo $person;
+
+				$query = "SELECT * FROM contacts"; 
+			    $result = mysqli_query($connection,$query);
+
+				echo "<table>"; 
+				echo '<tr><th>fName</th><th>lName</th><th>email</th></tr>';
+				while($row = mysqli_fetch_assoc($result)){  
+					echo "<tr><td>" . $row['fName'] . "</td><td>" . $row['lName'] . "</td><td>" . $row['mailAddress'] . "</td></tr>";  
+				}
+
+				echo "</table>";
+
+				break;
+			
+			default:
+				break;
+		}
 	}
 
-	echo "</table>";
-
-
-	mysqli_close($connection);
+	mysqli_close($connection);	//closing database connection
 ?>
