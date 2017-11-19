7018ac36bec97d7712d5ca81214568ff4e9550e7
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-19 21:25:59 +0530

    add information type

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 				
 				//fetch contact details according to information-type
 				if($infoType == 'email address'){
-					$query = "SELECT mailAddress FROM contacts WHERE fName = '$splitName[0]' and lName = '$splitName[1]'";
+					$query = "SELECT * FROM contacts WHERE fName = '$splitName[0]' and lName = '$splitName[1]'";
 					$result = mysqli_query($connection,$query);
 					$mail = mysqli_fetch_assoc($result);
 					$speech = "Email address of $person is {$mail['mailAddress']}" ;
