52a4c04cee5ff3e00404e9c5935a6840534ba1a4
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-19 21:30:10 +0530

    add information type

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 					$result = mysqli_query($connection,$query);
 					$mail = mysqli_fetch_assoc($result);
 					$speech = "Email address of $person is {$mail['mailAddress']}" ;
-				}else if($infoType ==''){
+				}else if($infoType == ''){
 					$query = "SELECT * FROM contacts WHERE fName = '$splitName[0]' and lName = '$splitName[1]'";
 					$result = mysqli_query($connection,$query);
 					$mail = mysqli_fetch_assoc($result);
