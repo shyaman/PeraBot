76ea9eb593ad14555f87960e55720238d8abd8ac
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-20 12:51:57 +0530

    Error handling

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php 
 	require_once('inc/connect.php'); //database connect
 	$method  = $_SERVER['REQUEST_METHOD'];
@@ -18,14 +19,33 @@
 				$result = mysqli_query($connection,$query);
 				$contactDetail = mysqli_fetch_assoc($result);
 				
-				if($infoType == 'email address'){
-					$speech = "Email address of $person is {$contactDetail['mailAddress']}" ;
-				}else if($infoType == ''){
-					$speech = "Email address : {$contactDetail['mailAddress']} \n Telephone number : {$contactDetail['phoneNumber']} " ;
-				}else if($infoType == 'telephone number'){
-					$speech = "Telephone number of $person is {$contactDetail['phoneNumber']}" ;
+				//fetch the result according to the information type
+				if($result != ''){
+					if($infoType == 'email address'){
+						if($contactDetail['mailAddress'] != ''){
+							$speech = "Email address of $person is {$contactDetail['mailAddress']}" ;
+						}else{
+							$speech = "Sorry ! $person's  email address is not there" ;
+						}
+					}else if($infoType == ''){
+						if($contactDetail['mailAddress'] != '' && $contactDetail['phoneNumber'] != ''){
+							$speech = "Email address : {$contactDetail['mailAddress']} \n Telephone number : {$contactDetail['phoneNumber']} " ;
+						}else if($contactDetail['mailAddress'] == ''){
+							$speech = "Telephone number : {$contactDetail['phoneNumber']} \n Sorry ! No email address was found" ;
+						}else if($contactDetail['phoneNumber'] == ''){
+							$speech = "Email address : {$contactDetail['mailAddress']} \n Sorry ! No Telephone number was found" ;
+						}
+					}else if($infoType == 'telephone number'){
+						if({$contactDetail['phoneNumber']} != ''){
+							$speech = "Telephone number of $person is {$contactDetail['phoneNumber']}" ;
+						}else{
+							$speech = "Sorry ! $person's  telephone number is not there" ;
+						}
+					}
+				}else{
+					$speech = "Sorry ! No such person in the contact list" ;
 				}
-
+				
 				//create reponse to the dilogflow and echo it
 				$response = new \stdClass();
 				$response->speech = $speech;
