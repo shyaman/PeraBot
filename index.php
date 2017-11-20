8789bcf33ae7301831559a1c8e731826d687c174
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-20 13:14:18 +0530

    Error handling

--- index.php
+++ index.php
@@ -31,9 +31,10 @@
 					if($contactDetail['mailAddress'] != '' && $contactDetail['phoneNumber'] != ''){
 						$speech = "Email address : {$contactDetail['mailAddress']} \n Telephone number : {$contactDetail['phoneNumber']} " ;
 					}else if($contactDetail['mailAddress'] == ''){
-						$speech = "Telephone number : {$contactDetail['phoneNumber']} \n Sorry ! No email address was found" ;
-					}else if($contactDetail['phoneNumber'] == ''){
 						$speech = "Email address : {$contactDetail['mailAddress']} \n Sorry ! No Telephone number was found" ;
+						
+					}else if($contactDetail['phoneNumber'] == ''){
+						$speech = "Telephone number : {$contactDetail['phoneNumber']} \n Sorry ! No email address was found" ;
 					}
 				}else if($infoType == 'telephone number'){
 					$speech = "Telephone number of $person is {$contactDetail['phoneNumber']}" ;
