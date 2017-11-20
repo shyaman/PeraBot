3ee54d5a4029dc205d91ada583bfd206c1abff16
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-20 13:15:54 +0530

    Error handling

--- index.php
+++ index.php
@@ -30,11 +30,8 @@
 				}else if($infoType == ''){
 					if($contactDetail['mailAddress'] != '' && $contactDetail['phoneNumber'] != ''){
 						$speech = "Email address : {$contactDetail['mailAddress']} \n Telephone number : {$contactDetail['phoneNumber']} " ;
-					}else if($contactDetail['mailAddress'] == ''){
-						$speech = "Email address : {$contactDetail['mailAddress']} \n Sorry ! No Telephone number was found" ;
-						
-					}else if($contactDetail['phoneNumber'] == ''){
-						$speech = "Telephone number : {$contactDetail['phoneNumber']} \n Sorry ! No email address was found" ;
+					}else{
+						$speech = "Sorry ! " ;
 					}
 				}else if($infoType == 'telephone number'){
 					$speech = "Telephone number of $person is {$contactDetail['phoneNumber']}" ;
