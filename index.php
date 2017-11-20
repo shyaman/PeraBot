9a0cd17418e8a53ead21ad8ed67f995349436267
Sandali Lokuge <sdewnilokuge@gmail.com>
2017-11-20 13:43:50 +0530

    Error handling

--- index.php
+++ index.php
@@ -36,11 +36,7 @@
 						$speech = "Telephone number : {$contactDetail['phoneNumber']} \n Sorry ! No email address was found" ;
 					}
 				}else if($infoType == 'telephone number'){
-					if({$contactDetail['phoneNumber']} != ''){
-						$speech = "Telephone number of $person is {$contactDetail['phoneNumber']}" ;
-					}else{
-						$speech = "Sorry ! $person's  telephone number is not there" ;
-					}
+					$speech = "Telephone number of $person is {$contactDetail['phoneNumber']}" ;
 				}
 				
 				//create reponse to the dilogflow and echo it
